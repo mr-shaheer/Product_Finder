3ef4d5f6d28f2c8a63fa83905bf45d2e1433bd48
Shaheer <mrshaheer.info@gmail.com>
2026-07-18 21:49:37 +0500

    update LLM Model configuration with Gemini support

--- models.py
+++ models.py
@@ -2,19 +2,19 @@ import os
 from agents import OpenAIChatCompletionsModel, AsyncOpenAI
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv)
+load_dotenv(find_dotenv())
 
 external_client = AsyncOpenAI(
-     api_key = os.get_env("GEMINI_API_KEY"),
+     api_key = os.getenv("GEMINI_API_KEY"),
      base_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
 Default_Model = OpenAIChatCompletionsModel(
      model = "gemini-2.5-flash",
-     openao_client = external_client
+     openai_client = external_client
 )
 
 Frontier_Model = OpenAIChatCompletionsModel(
-     model = "gemini-2.5-flash",
-     openao_client = external_client
+     model = "gemini-3.5-flash",
+     openai_client = external_client
 )
